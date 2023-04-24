b31869aeab39efc9d976c86b7808b532fe48fcb9
Kai05071234 <84474510+Kai05071234@users.noreply.github.com>
2023-04-24 20:07:28 +0100

    Update main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+function preload() {
+  ball_touch_paddel = loadSound("ball_touch_paddel.wav");
+  ball_missed = loadSound("missed.wav");
+}
+function setup(){
+  var canvas =  createCanvas(700,600);
+  canvas.parent("canvas");
+  video = createCapture(VIDEO);
+  video.size(700, 600);
+  video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+function modelLoaded() {
+  console.log("model is loaded!");
+}
+function gotPoses(results) {
+  if (results.length > 0) {
+    console.log(results);
+    rightWrist_x = results[0].pose.rightWrist.x;
+    rightWrist_y = results[0].pose.rightWrist.y;
+    rightWrist_score = results[0].pose.keypoints[10].score;
+    console.log("right wrist x = " + rightWrist_x + " | right wrist y = " + rightWrist_y + " | right wrist score " + rightWrist_score);
+  }
+}
