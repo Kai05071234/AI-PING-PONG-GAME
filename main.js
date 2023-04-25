11e85a33ced807c65c06565d0061b4999ee8991b
Kai05071234 <84474510+Kai05071234@users.noreply.github.com>
2023-04-25 20:13:41 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ rightWrist_x = 0;
 rightWrist_score = "";
 
 function setup(){
-  canvas =  createCanvas(700,600);
   canvas.parent("canvas");
   video = createCapture(VIDEO);
   video.size(700, 600);
